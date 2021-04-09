080c79b94e2d3147edb2fc54edd418cb0e729905
Raarity <gtoshev2@mail.ru>
2021-04-09 12:20:34 +0300

    task from TUmakov

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import pylab
 from scipy.ndimage import maximum_filter, minimum_filter
 from matplotlib import pyplot as plt
 
+
+# отыскать источник порождения артефактов (источник вторичных волн
+# четко находить источник!!!
+# пн или вт или в конце недели
+# полярная система координат попробовать преобразить
+
 def viewImage(image):
     cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
     cv2.imshow('Display', image)
@@ -39,37 +45,37 @@ def print_hi(name):
 
     # Perform binary thresholding on the image with T = 125
     # r, threshold = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
-    #viewImage(threshold)
+    # viewImage(threshold)
 
             # Арифметический фильтр с Sharpening Kernel
-    # # Create our sharpening kernel, the sum of all values must equal to one for uniformity
-    # kernel_sharpening = np.array([[-1, -1, -1],
-    #                               [-1, 9, -1],
-    #                               [-1, -1, -1]])
-    #
-    # # Applying the sharpening kernel to the grayscale image &amp; displaying it.
-    # print("\n\n--- Effects on S&amp;P Noise Image with Probability 0.5 ---\n\n")
-    #
-    # # Applying filter on image with salt &amp; pepper noise
-    #
-    # sharpened_img = cv2.filter2D(img, -1, kernel_sharpening)
-    # viewImage(sharpened_img)
+    # Create our sharpening kernel, the sum of all values must equal to one for uniformity
+    kernel_sharpening = np.array([[-1, -1, -1],
+                                  [-1, 9, -1],
+                                  [-1, -1, -1]])
 
-    # kernel_gausian = np.array([[1/16, 1/8, 1/16],
-    #                               [1/8, 1/4, 1/8],
-    #                               [1/16, 1/8, 1/16]])
-    # gaus_img=cv2.filter2D(img, -1, kernel_gausian, borderType=cv2.BORDER_CONSTANT)
-    # viewImage(gaus_img)
+    # Applying the sharpening kernel to the grayscale image &amp; displaying it.
+    print("\n\n--- Effects on S&amp;P Noise Image with Probability 0.5 ---\n\n")
+
+    # Applying filter on image with salt &amp; pepper noise
+
+    sharpened_img = cv2.filter2D(img, -1, kernel_sharpening)
+    viewImage(sharpened_img)
+
+    kernel_gausian = np.array([[1/16, 1/8, 1/16],
+                                  [1/8, 1/4, 1/8],
+                                  [1/16, 1/8, 1/16]])
+    gaus_img=cv2.filter2D(img, -1, kernel_gausian, borderType=cv2.BORDER_CONSTANT)
+    viewImage(gaus_img)
 
 
 
         #Гаусов блюр opencv
-    # blur = cv2.GaussianBlur(img, (5, 5), 0)
+    # blur = cv2.GaussianBlur(img, (5,5), 0)
     # viewImage(blur)
 
             # какой то устранитель шума из ореncv
     # dst = cv2.fastNlMeansDenoising(img, None, 10, 10, 7, 21)
-
+    # viewImage(dst)
         # шо це за дич хз прост исправило задник
     # se = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
     # bg = cv2.morphologyEx(img, cv2.MORPH_DILATE, se)
